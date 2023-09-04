e89f20ad1a7c69679eb7248319d03d7c7ceb1ec1
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-03 20:49:52 -0500

    Style YouTube embed of 'RoboCop' trailer.

--- src/components/RoboCopTrailer.js
+++ src/components/RoboCopTrailer.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
 const RoboCopTrailer = () => {
+
   return (
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/6tC_5mp3udE?si=0RhbhW691d3slwBw"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
+    <div className="video-responsive">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/6tC_5mp3udE?si=0RhbhW691d3slwBw"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
       />
+    </div>
   );
 }
 
