00817a94adb11575884134dc18ec451075bf951e
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-04 21:23:26 -0500

    Import images into header component. Change wrapper to header tags. Set images as return expression.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react"
+import FoggyNeonCity from "../backgrounds/foggyneoncity.jpg"
+import OCPCompanyLogo from "../images/ocp_company_logo.jpeg"
 
 const Header = () => {
   return (
-    <div>OCP Header</div>
-  )
+    <header>
+      <img src={FoggyNeonCity} alt="City backdrop" />
+      <img src={OCPCompanyLogo} alt="OCP company logo" />
+    </header>
+  );
 }
 
 export default Header
