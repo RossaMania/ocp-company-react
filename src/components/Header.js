b8f541537ddad50ed4c5079716a0faefff15d17d
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-04 22:12:07 -0500

    Add text in header. Wrap in div with id of header-content. Set z-index of this div to 1.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,22 @@ import OCPCompanyLogo from "../images/ocp_company_logo.jpeg"
 const Header = () => {
   return (
     <header>
-      <img src={FoggyNeonCity} alt="City backdrop" id="header-city"/>
-      <img src={OCPCompanyLogo} alt="OCP company logo" id="header-logo"/>
+      <img src={FoggyNeonCity} alt="City backdrop" id="header-city" />
+      <img src={OCPCompanyLogo} alt="OCP company logo" id="header-logo" />
+      <div id="header-content">
+        <h1>
+          It's time we gave something back.
+          <strong>Good business is where you find it.</strong>
+        </h1>
+        <h2>
+          Every citizen will have a living unit.
+          <strong>Safe, secure, and clean.</strong>
+        </h2>
+        <h2>
+          We need 24-hour-a-day police.
+          <strong>Cops who don't eat or sleep.</strong>
+        </h2>
+      </div>
     </header>
   );
 }
